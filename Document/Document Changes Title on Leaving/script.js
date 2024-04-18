94a88e59422668c5f7af24844580c0da09fa7401
eshan-sud <eshansud22@gmail.com>
2024-04-18 18:59:47 +0530

    .

--- Document/Document Changes Title on Leaving/script.js
+++
@@ -1,3 +0,0 @@
-let docTitle = document.title;
-window.addEventListener("blur", () => {document.title = "Leaving So Soon...🥺";})
-window.addEventListener("focus", () => {document.title = docTitle;})
